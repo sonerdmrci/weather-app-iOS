932f0edd10a49bf091ede5843e6ba99e8578b9a3
tp.sdemirc <soner.demirci@tein.com.tr>
2023-11-28 09:45:48 +0300

    Initial Commit

---
+++ WeatherAppProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppProject
+//
+//  Created by Soner Demirci on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
